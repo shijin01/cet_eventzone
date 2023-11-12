42c4c8148375f1ffd6e96cc7e446803a26a12768
shijin01 <shijinshajicrnl@gmail.com>
2023-11-13 01:06:27 +0530

    Update eventpage.dart

--- lib/components/eventpage.dart
+++ lib/components/eventpage.dart
@@ -10,44 +10,45 @@ class EventWidget extends StatefulWidget {
 }
 
 class _EventWidgetState extends State<EventWidget> {
-  final SupabaseStreamBuilder _stream =
-      supabase.from('events').stream(primaryKey: ['id']);
+  final SupabaseStreamBuilder _stream =supabase.from('events').stream(primaryKey: ['id']);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<Map<String, dynamic>>>(
-      stream: _stream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
-
-        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          List<Map<String, dynamic>> data =
-              snapshot.data as List<Map<String, dynamic>>;
-          return Column(children: [
-            Text("SearchBar"),
-            ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(data[index]['event_name'] +
-                            " " +
-                            data[index]['event_date'] ??
-                        " "),
-                    subtitle: Text(data[index]['department']),
-                    // trailing: Icon(Icons.more_vert),
-                    isThreeLine: true,
-                  ),
-                );
-              },
-            ),
-          ]);
-        } else {
-          return Text("loading");
-        }
-      },
+    return Column(
+      children: <Widget>[
+        const Text("Helo"
+        ),
+        StreamBuilder<List<Map<String, dynamic>>>(
+          stream: _stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+            if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              List<Map<String, dynamic>> data =
+                  snapshot.data as List<Map<String, dynamic>>;
+              return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(data[index]['event_name'] +
+                              " " +
+                              data[index]['event_date'] ??
+                          " "),
+                      subtitle: Text(data[index]['department']),
+                      // trailing: Icon(Icons.more_vert),
+                      isThreeLine: true,
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Text("loading");
+            }
+          },
+        ),
+      ],
     );
   }
 }
