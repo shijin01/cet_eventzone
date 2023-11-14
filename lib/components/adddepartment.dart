7b1eed930e265ecbdadcfdf495979d0b6d2d0bd7
shijin01 <shijinshajicrnl@gmail.com>
2023-11-14 23:26:39 +0530

    not a proper update

--- lib/components/adddepartment.dart
+++ lib/components/adddepartment.dart
@@ -1,3 +1,4 @@
+import 'package:cet_eventzone/dbconnect.dart';
 import 'package:flutter/material.dart';
 
 class AddDepartment extends StatefulWidget {
@@ -17,11 +18,12 @@ class _AddDepartmentState extends State<AddDepartment> {
   int? year;
 
   // var dropvalue = droplist[0];
-  bool showpassword = false;
+  bool showpassword = true;
 
   @override
   Widget build(BuildContext context) {
     const List<int> droplist = <int>[1, 2, 3, 4, 5];
+    int selectedyear = 1;
     final List<DropdownMenuEntry<int>> dropdownmenuentries =
         <DropdownMenuEntry<int>>[];
     for (int i in droplist) {
@@ -32,87 +34,114 @@ class _AddDepartmentState extends State<AddDepartment> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
-        controller: emailcontroller,
-        decoration: const InputDecoration(
-            hintText: "Email",
-            prefixIcon: Icon(Icons.email),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-      ),
-    ),
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
-        obscureText: showpassword,
-        decoration: InputDecoration(
-            hintText: "Password",
-            suffixIcon: IconButton(
-                onPressed: () {
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            controller: emailcontroller,
+            decoration: const InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            obscureText: showpassword,
+            decoration: InputDecoration(
+                hintText: "Password",
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showpassword = !showpassword;
+                      });
+                    },
+                    icon: showpassword
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off)),
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+            controller: passwordcontroller,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            controller: namecontroller,
+            decoration: const InputDecoration(
+                hintText: "Name",
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            controller: departmentcontroller,
+            decoration: const InputDecoration(
+                hintText: "Department",
+                prefixIcon: Icon(Icons.home_work_rounded),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("Year:"),
+              ),
+              DropdownMenu<int>(
+                initialSelection: selectedyear,
+                dropdownMenuEntries: dropdownmenuentries,
+                controller: yearcontroller,
+                onSelected: (i) {
                   setState(() {
-                    showpassword = !showpassword;
+                    year = i;
                   });
                 },
-                icon: showpassword
-                    ? Icon(Icons.visibility)
-                    : Icon(Icons.visibility_off)),
-            border: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-        controller: passwordcontroller,
-      ),
-    ),
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
-        controller: namecontroller,
-        decoration: const InputDecoration(
-            hintText: "Name",
-            prefixIcon: Icon(Icons.person),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-      ),
-    ),
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
-        controller: departmentcontroller,
-        decoration: const InputDecoration(
-            hintText: "Department",
-            prefixIcon: Icon(Icons.home_work_rounded),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-      ),
-    ),
-    Container(
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(5),
-            child: const Text("Year:"),
+              )
+            ],
           ),
-          DropdownMenu<int>(
-            initialSelection: 1,
-            dropdownMenuEntries: dropdownmenuentries,
-            controller: yearcontroller,
-            onSelected: (i) {
-              setState(() {
-                year = i;
-              });
+        ),
+        ElevatedButton(
+            onPressed: () async {
+              final isusercreated = await createuser(
+                  emailcontroller.text, passwordcontroller.text);
+              if (isusercreated) {
+                final lid =
+                    await insertintologin(emailcontroller.text, "department");
+                final success = await insertintouserdetails(lid,
+                    namecontroller.text, departmentcontroller.text, year ?? 0);
+                if (success) {
+                  // ignore: use_build_context_synchronously
+                  emailcontroller.text = "";
+                  passwordcontroller.text = "";
+                  departmentcontroller.text = "";
+                  yearcontroller.text = "1";
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.green[200],
+                      content: const Text("Successfully added")));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.red[300],
+                      content: const Text(
+                          "User registered ,But there is something issue occured")));
+                }
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    backgroundColor: Colors.red,
+                    content: Text("Error occured")));
+                //
+              }
+
+              //
             },
-          )
-        ],
-      ),
-    ),
-    ElevatedButton(
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text(
-                  "email:${emailcontroller.text}\npassword:${passwordcontroller.text}\nname:${namecontroller.text}\ndepartment:${departmentcontroller.text}\nyear:$year\n")));
-        },
-        child: const Text("REGISTER"))
+            child: const Text("REGISTER"))
       ],
     );
   }
--- lib/dbconnect.dart
+++ lib/dbconnect.dart
@@ -4,14 +4,12 @@ import 'package:cet_eventzone/main.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-Future<List> createuser(String email, String password) async {
+Future<bool> createuser(String email, String password) async {
   final AuthResponse res = await supabase.auth.signUp(
     email: 'example@email.com',
     password: 'example-password',
   );
-  final Session? session = res.session;
-  final User? user = res.user;
-  return [session, user];
+  return res.user.toString()==email ;
 }
 
 Future<int> insertintologin(String username, String typeofuser) async {
@@ -25,10 +23,9 @@ Future<bool> insertintouserdetails(
     int lid, String name, String department, int year) async {
   bool success = true;
   try {
-    final List<Map<String, dynamic>> data =
-        await supabase.from('login').insert([
+    await supabase.from('login').insert([
       {'lid': lid, 'name': name, "department": department, "year": year},
-    ]).select();
+    ]);
   } catch (err) {
     success = false;
   } finally {
